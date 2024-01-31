6bd384c82b5da7487864354e2e936538419abd59
BommaleniPavanKalyan <143407333+BommaleniPavanKalyan@users.noreply.github.com>
2024-01-31 15:03:19 +0530

    Add files via upload

---
+++ ProjectWork/Hospital_Servlet/src/main/java/com/org/servlet/user/UserRegister.java
@@ -0,0 +1,40 @@
+package com.org.servlet.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.org.dao.UserDao;
+import com.org.entity.User;
+
+@WebServlet("/user_register")
+public class UserRegister extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String fullName=req.getParameter("fullname");
+		String email=req.getParameter("email");
+		String password=req.getParameter("password");
+		
+		User u = new User(fullName, email, password);
+		UserDao dao=new UserDao();
+		boolean res=dao.registerUser(u);
+		HttpSession hs=req.getSession();
+		
+		if(res) {
+			hs.setAttribute("sucMsg","SIGNUP SUCCESS");
+			resp.sendRedirect("signup.jsp");
+		}else {
+			hs.setAttribute("errorMsg","SIGNUP FAILED");
+			resp.sendRedirect("signup.jsp");
+		}
+		
+		
+		
+	}
+}
